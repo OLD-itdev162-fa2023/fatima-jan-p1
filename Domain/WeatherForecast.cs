a41b337c69ff5d779cd605d1f4dbc465fa4b9cbf
unknown <janf@gmatc.matc.edu>
2022-10-03 08:16:59 -0500

    Project 1 Completed

--- Domain/WeatherForecast.cs
+++ Domain/WeatherForecast.cs
@@ -5,7 +5,6 @@ public class WeatherForecast
 {
     public int Id { get; set;}
     public DateTime Date {get; set;}
-
     public int TemperatureC {get ; set;}
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
